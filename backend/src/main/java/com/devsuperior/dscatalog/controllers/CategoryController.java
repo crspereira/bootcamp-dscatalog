537eeaf92eb1bee99f722982dde969b4897b01d6
Clayton Pereira <clayton.pereira@outlook.com>
2022-03-28 12:40:12 -0300

    BackEnd Cap-1 - Added CategoryRepositoy and H2 DataBase

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,24 +1,26 @@
 package com.devsuperior.dscatalog.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
 	
+	@Autowired
+	private CategoryService categoryService;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> categories = new ArrayList<Category>();
-		categories.add(new Category(1L, "Books"));
-		categories.add(new Category(2L, "Eletronics"));
+		List<Category> categories = categoryService.findAll();
 		
 		return ResponseEntity.ok().body(categories);
 	}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+  /*//Injeção de Dependencia Manual via Construtor
+	private CategoryRepository categoryRepository;
+	
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}*/
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List<Category> findAll() {
+		return categoryRepository.findAll();
+	}
+
+}
